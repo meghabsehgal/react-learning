8bd5585f3f721055e980f683a9e90e038200488a
Megha Sehgal <66651393+meghabsehgal@users.noreply.github.com>
2023-07-03 07:40:18 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//var React = require("react");
+//var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>paragraph</p>
+    <ul>
+      <li>Megha</li>
+      <li>Nishu</li>
+      <li>Vicky</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
